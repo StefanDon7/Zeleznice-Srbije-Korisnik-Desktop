b094b0a972ab65ba9141ef51d73acd8ecbe6141b
StefanDon7 <58365033+StefanDon7@users.noreply.github.com>
2020-05-06 02:31:37 +0200

    prvi komit

---
+++ src/main/java/rs/stefanlezaic/zeleznice/srbije/klijent/kontroler/Kontroler.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.stefanlezaic.zeleznice.srbije.klijent.kontroler;
+
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.Klijent;
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.MedjuStanica;
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.Polazak;
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.Rezervacija;
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.Stanica;
+import java.util.ArrayList;
+import rs.stefanlezaic.zeleznice.srbije.klijent.komunikacija.KomunikacijaSaServerom;
+import rs.stefanlezaic.zeleznice.srbije.lib.kons.Konstante;
+import rs.stefanlezaic.zeleznice.srbije.lib.kons.ResponseStatus;
+import rs.stefanlezaic.zeleznice.srbije.lib.transfer.KlijentskiZahtev;
+import rs.stefanlezaic.zeleznice.srbije.lib.transfer.ServerskiOdgovor;
+
+/**
+ *
+ * @author sleza
+ */
+public class Kontroler {
+
+    private static Kontroler instance;
+    private KlijentskiZahtev kz;
+
+    private Kontroler() {
+    }
+
+    public static Kontroler getInstance() {
+        if (instance == null) {
+            instance = new Kontroler();
+        }
+        return instance;
+    }
+
+    public Klijent UlogujSe(Klijent k) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setParametar(k);
+        kz.setOperacija(Konstante.PRIJAVLJIVANJE);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+        return (Klijent) so.getOdgovor();
+    }
+
+    public void RegistrujSe(Klijent k) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.REGISTRACIJA);
+        kz.setParametar(k);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+    }
+
+    public void rezervisiPolazak(Rezervacija r) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.REZERVISI_POLAZAK);
+        kz.setParametar(r);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+    }
+
+    public ArrayList<Rezervacija> vratiMojeRezervacije(Rezervacija r) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.VRATI_REZERVACIJE_ZA_KLIJENTA);
+        kz.setParametar(r);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        ArrayList<Rezervacija> lista = (ArrayList<Rezervacija>) so.getOdgovor();
+        return lista;
+    }
+
+    public void IzmeniNalog(Klijent klijent) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.IZMENA_KORISNICKOG_NALOGA);
+        kz.setParametar(klijent);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+    }
+
+    public int vratiBrojRezervacija(Rezervacija r) throws Exception {
+        int broj = 0;
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.VRATI_REZERVACIJE_ZA_POLAZAK);
+        kz.setParametar(r);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        broj = ((ArrayList<Rezervacija>) so.getOdgovor()).size();
+        return broj;
+    }
+
+    public void otkaziRezervaciju(Rezervacija r) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.OTKAZI_REZERVACIJU);
+        kz.setParametar(r);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+    }
+
+    public ArrayList<MedjuStanica> vratiMiSveMedjustanica() throws Exception {
+        KlijentskiZahtev kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.VRATI_MEDJUSTANICE);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        ArrayList<MedjuStanica> listaMedjuStanica = (ArrayList<MedjuStanica>) so.getOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+        return listaMedjuStanica;
+    }
+
+    public ArrayList<MedjuStanica> vratiMiMedjustaniceLiniju(MedjuStanica medjuStanica) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.VRATI_MEDJUSTANICE_LINIJE);
+        kz.setParametar(medjuStanica);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        ArrayList<MedjuStanica> listaMedjustanica = (ArrayList<MedjuStanica>) so.getOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+        return listaMedjustanica;
+    }
+
+    public ArrayList<Stanica> vratiMiSveStanice() throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.VRATI_STANICE);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        ArrayList<Stanica> listaStanica = (ArrayList<Stanica>) so.getOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+        return listaStanica;
+    }
+
+    public ArrayList<Polazak> vratiMiPolaskeZaDatum(Polazak p) throws Exception {
+        kz = new KlijentskiZahtev();
+        kz.setOperacija(Konstante.VRATI_POLASKE);
+        kz.setParametar(p);
+        KomunikacijaSaServerom.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so = KomunikacijaSaServerom.getInstance().primiOdgovor();
+        ArrayList<Polazak> listaPolazaka = (ArrayList<Polazak>) so.getOdgovor();
+        if (so.getStatus() == ResponseStatus.ERROR) {
+            Exception ex = (Exception) so.getError();
+            throw ex;
+        }
+        return listaPolazaka;
+    }
+
+}
---
+++ src/main/java/rs/stefanlezaic/zeleznice/srbije/klijent/modeli/tabela/ModelTabelePolasci.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.stefanlezaic.zeleznice.srbije.klijent.modeli.tabela;
+
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.Polazak;
+import rs.stefanlezaic.zeleznice.srbije.lib.domen.Rezervacija;
+import rs.stefanlezaic.zeleznice.srbije.klijent.kontroler.Kontroler;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author sleza
+ */
+public class ModelTabelePolasci extends AbstractTableModel {
+
+    SimpleDateFormat smfDatum = new SimpleDateFormat("dd.MM.yyyy");
+    SimpleDateFormat smfVreme = new SimpleDateFormat("HH:mm");
+    String[] kolone = {"Linija", "Polazak", "Datum polaska", "Dolazak", "Datum dolaska", "Putuje", "Rang", "Rezervisano/Mesta", "Napomena"};
+    ArrayList<Polazak> list = new ArrayList<>();
+
+    @Override
+    public int getRowCount() {
+        if (list == null) {
+            return 0;
+        }
+        return list.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return kolone.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Polazak p = list.get(rowIndex);
+        Rezervacija r = new Rezervacija(null, p, null);
+        switch (columnIndex) {
+            case 0:
+                return p.getLinija().getNaziv();
+            case 1:
+                return smfVreme.format(p.getDatumPolaska());
+            case 2:
+                return smfDatum.format(p.getDatumPolaska());
+            case 3:
+                return smfVreme.format(p.getDatumDolaska());
+            case 4:
+                return smfDatum.format(p.getDatumDolaska());
+            case 5:
+                //vrati vreme putovanja
+                int vreme = p.getLinija().getMinutaza();
+                int sati = vreme / 60;
+                int minuti = vreme % 60;
+                if (sati == 0) {
+                    return minuti + "min";
+                }
+                if (minuti == 0) {
+                    return sati + "h";
+                }
+                return sati + "h i " + minuti + "min";
+            case 6:
+                return p.getLinija().getTipLinije().getNaziv();
+            case 7:
+                return vratiBroj(r) + "/" + p.getVoz().getBrojSedista();
+            case 8:
+                return p.getNapomena();
+            default:
+                return "Cao zdravo!";
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        for (int i = 0; i < kolone.length; i++) {
+            return kolone[column];
+        }
+        return "Cao";
+    }
+
+    public void dodajPredmet(Polazak p) {
+        list.add(p);
+        fireTableDataChanged();
+    }
+
+    public void obrisiIzListe(int i) {
+        list.remove(i);
+        fireTableDataChanged();
+    }
+
+    public ArrayList<Polazak> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Polazak> list) {
+        this.list = list;
+        fireTableDataChanged();
+    }
+
+    public void ocistiTabelu() {
+        list = new ArrayList<>();
+        fireTableDataChanged();
+    }
+
+    public ArrayList<Polazak> vratiListu() {
+        return list;
+    }
+
+    public int vratiBroj(Rezervacija r) {
+        int broj = 0;
+        try {
+            return Kontroler.getInstance().vratiBrojRezervacija(r);
+        } catch (Exception ex) {
+            return broj;
+        }
+    }
+
+    public boolean popunjeno(Rezervacija r) {
+        if (vratiBroj(r) == r.getPolazak().getVoz().getBrojSedista()) {
+            return true;
+        }
+        return false;
+    }
+
+}
